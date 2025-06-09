3288cc86cf25b7dfea9f2114023707577e82c676
shivaamsingh <shivamsinghh088@gamil.com>
2025-06-09 09:19:24 +0530

    new files

---
+++ 13_dowhile.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1;
+    do
+    {
+        cout<<"WOOF\n";
+        i++;
+    } while (i<=10);
+    
+
+
+
+    return 0;
+}
---
+++ 14_functios.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+// void = which does not return anything
+// return 
+// parameterize  
+// non parameterize
+void printName(string name){
+    cout<<name;
+}
+int main(){
+    string name;
+    cin>>name;
+    printName(name);
+
+    return 0;
+}

b99740761ff1d1bc68f330f8382713082995a3ae
shivaamsingh <shivamsinghh088@gamil.com>
2025-06-09 09:58:05 +0530

    new files

--- 15_wap_fsum.cpp
+++ 15_wap_fsum.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
-int sum(int num1,int num2){
+void sum(int num1,int num2){
     int num3=num1+num2;
-    return num3;
+    cout<< num3;
 }
 int main(){
     int num1,num2;
     cin>>num1>>num2;
-    int res=sum(num1,num2);
-    cout<<res;
+    sum(num1,num2);
+
 
 
 

dfffb9bfce2c8321b58dc22735f4a8e912cf8467
shivaamsingh <shivamsinghh088@gamil.com>
2025-06-17 14:56:24 +0530

    new files

---
+++ 03_STL/01.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+void print(){
+    cout<<"bbo bbo"<<endl;
+}
+
+int sum(int a , int b){
+    return a+b;
+}
+int main()
+{
+    int a;
+    print();
+    int s= sum(1,5);
+    cout<<s;
+
+
+
+
+    return 0;
+}
---
+++ 03_STL/02_pairs.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void explainPair(){
+    pair<int,int> p={1,3};
+     
+    cout<<p.first<<" "<<p.second;
+    
+}
+
+int main() {
+    
+    return 0;
+}
